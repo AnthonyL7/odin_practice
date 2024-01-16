f7ec42a210d0ead00568bb40be8f09f5cd6e41b4
AnthonyL7 <anthonylanda022@gmail.com>
2024-01-16 15:33:50 -0800

    Make a Folder
    Create a Directory for Javascript and finish the Assignment for the foundations part 1

---
+++ jscript-problems/lesson-1/lesson-1.js
@@ -0,0 +1,27 @@
+let name = "John";
+let admin = name;
+console.log(admin);
+
+// Giving the right name
+let planet = "earth";
+let currentVisitor = "John";
+console.log(planet, currentVisitor);
+
+// Foundations Part 1 Assignment
+// 1.
+console.log(23+97);
+// 2.
+console.log(23+97+50+89+34+12);
+// 3.
+console.log((4+6+9)/77);
+// 4.
+let a = 10;
+console.log(a);
+console.log(9*a);
+let b = 7 * a;
+console.log(b);
+// 5. 
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log(percentage);
