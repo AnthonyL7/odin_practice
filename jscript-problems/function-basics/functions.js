14dbebc6910228bb0ed2b5db8cbc0e6c08f9f85c
AnthonyL7 <anthonylanda022@gmail.com>
2024-03-02 13:52:36 -0800

    Add files

---
+++ jscript-problems/function-basics/functions.js
@@ -0,0 +1,17 @@
+//#1
+
+function add7() {
+  let num = 7;
+  return num + 7;
+}
+
+console.log(add7());
+
+//#2
+function multiply() {
+  let num1 = 7;
+  let num2 = 5;
+  return num1 * num2;
+}
+
+console.log(multiply());
