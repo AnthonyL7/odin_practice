4c0011cf97a00b9f7c87ff7ad465947a3c205783
AnthonyL7 <anthonylanda022@gmail.com>
2024-04-11 19:39:26 -0700

    Finish Small exercise

--- jscript-problems/DOM-practice/script.js
+++ jscript-problems/DOM-practice/script.js
@@ -19,3 +19,26 @@ h3.style.color= "blue";
 container.appendChild(h3);
 
 
+// div
+let div = document.createElement("div");
+div.classList.add("div");
+div.innerText = "Ima div";
+div.style.border = "10px solid blue";
+div.style.backgroundColor = "pink";
+
+container.appendChild(div)
+
+
+// h1 inside div
+let h1 = document.createElement("h1");
+div.classList.add("h1");
+h1.innerText = "I'm in a div";
+h1.style.color = "White";
+
+div.appendChild(h1);
+
+let p2 = document.createElement("p");
+div.classList.add("p2");
+p2.innerText = "ME TOO!";
+
+div.appendChild(p2);
