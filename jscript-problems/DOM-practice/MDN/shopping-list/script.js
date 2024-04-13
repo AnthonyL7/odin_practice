d51b42350926320b49460cadf1b666090cb500dd
AnthonyL7 <anthonylanda022@gmail.com>
2024-04-12 19:15:02 -0700

    Finish exercise

---
+++ jscript-problems/DOM-practice/MDN/shopping-list/script.js
@@ -0,0 +1,8 @@
+let ul = document.querySelector("ul");
+let input = document.querySelector("input");
+let button = document.querySelector("button");
+
+function run () {
+  document.querySelector("button").addEventListener("click", displayDate)
+  let value = input;
+}
