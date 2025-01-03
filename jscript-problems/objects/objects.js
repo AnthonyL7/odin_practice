2b33260a1e28073651145e28383107a71e32cd5f
AnthonyL7 <anthonylanda022@gmail.com>
2025-01-03 11:03:58 -0800

    Add function to filter an array given a certain range

--- jscript-problems/objects/objects.js
+++ jscript-problems/objects/objects.js
@@ -1,18 +1,5 @@
-function camelize(str) {
-  let words = str.split('-')
-  let results = words[0];
-
-  for (let i = 1; i < words.length; i++) {
-    results += words[i].charAt(0).toUpperCase() + words[i].slice(1);
-  }
-  return results
+function filterRange(arr, a, b) {
+  return arr.filter(num => num >= a && num <= b )
 }
 
-result = camelize('background-color')
-console.log(result)
-
-word = 'anthony'
-console.log(word.slice(1))
-for (let i = 0; i < word.length; i++) {
-  console.log(word[i])
-}
+console.log(filterRange([5,3,8,1], 1, 4))
